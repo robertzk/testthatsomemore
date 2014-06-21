2fb107543e5db473ac093b5529eddae1cf6b4684
RobertZK <technoguyrob@gmail.com>
2014-06-20 23:14:57 -0500

    initial stub method

---
+++ R/stub.R
@@ -0,0 +1,16 @@
+#' Stub the environment of a closure.
+#'
+#' When testing functions, sometimes we would like to not use certain
+#' downstream methods that get called by that function. For example, if
+#' we are testing an HTTP library, we may not want to make actual HTTP calls.
+#' We can "stub" away the functions in our main function so they perform
+#' a less complex but functionally similar behavior. 
+#'
+#' @param fn function. The function to stub.
+#' @param key function. The variable/key in the closure to stub away.
+#' @param value ANY. The new value of the stubbed key.
+#' @examples
+#' fn <- function() { cat(paste0('a', 'b')) }
+#' stub(fn, 'paste0') <- function(...) base::paste(...)
+#' fn() # Will print "a b" instead of "ab"
+NULL
