e3391342a2d2d36346b1afbed2608e326a04d3d4
RobertZK <technoguyrob@gmail.com>
2014-06-20 22:06:15 -0500

    fix assert tests and make tests directory

--- inst/tests/test-assert.R
+++ inst/tests/test-assert.R
@@ -1,10 +1,14 @@
 context('assert')
 
-test_that('it lets no error go through', {
-  assert(TRUE)
-})
+local({
+  environment(assert)$expect_false <- function(x) if (identical(x, TRUE)) stop()
 
-test_that('it fails on an error', {
-  expect_error(assert(stop()))
-})
+  test_that('it lets no error go through', {
+    assert(TRUE)
+  })
 
+  test_that('it fails on an error', {
+    expect_error(assert(stop()))
+  })
+
+})
