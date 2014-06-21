bea62452b51ded9786d042ddc8a2ffcbfc93c0d3
RobertZK <technoguyrob@gmail.com>
2014-06-20 23:33:24 -0500

    allow nonstandard evaluation as well for stubbing

--- R/stub.R
+++ R/stub.R
@@ -7,14 +7,21 @@
 #' a less complex but functionally similar behavior. 
 #'
 #' @param fn function. The function to stub.
-#' @param key function. The variable/key in the closure to stub away.
+#' @param key character. The variable/key in the closure to stub away. One
+#'    can also specify the name (without quoting it, using non-standard
+#'    evaluation).
 #' @param value ANY. The new value of the stubbed key.
 #' @examples
 #' fn <- function() { cat(paste0('a', 'b')) }
 #' stub(fn, 'paste0') <- function(...) base::paste(...)
 #' fn() # Will print "a b" instead of "ab"
+#' stub(fn, paste0) <- function(...) base::paste(...) # This also works.
 `stub<-` <- function(fn, key, value) {
   environment(fn) <- new.env(parent = environment(fn)) 
+  # If key is a string, use that, otherwise deparse and substitute.
+  key <- 
+    if (is.element(substring(tmp <- deparse(substitute(key)), 1, 1), c("'", '"'))) key
+    else tmp
   assign(key, value, envir = environment(fn))
   fn
 }
