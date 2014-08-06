afdae1f861c2d396fa427dc3dbcb87ed7b51797c
RobertZK <technoguyrob@gmail.com>
2014-08-05 20:15:32 -0500

    touch function for touching files

--- R/utils.R
+++ R/utils.R
@@ -3,3 +3,13 @@
 #' @export
 is.directory <- function(x) file.exists(x) && file.info(x)$isdir
 
+#' Touch a file.
+#'
+#' Using the touch helper on a Unix system.
+#'
+#' @param filename character. The file to touch.
+#' @export
+touch <- function(filename) {
+  system(paste('touch', filename))
+}
+
