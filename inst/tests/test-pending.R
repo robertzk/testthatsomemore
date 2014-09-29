186ce5aa5406c2f466608cf751899c654fa533fb
RobertZK <technoguyrob@gmail.com>
2014-09-29 12:10:36 -0500

    Fix pending test for travis

--- inst/tests/test-pending.R
+++ inst/tests/test-pending.R
@@ -1,5 +1,13 @@
 context('pending')
 
+term <- Sys.getenv()["TERM"]
+colour_terms <- c("xterm-color", "xterm-256color", "screen",
+                        "screen-256color")
+
 test_that('it writes a yellow dot', {
-  expect_output(pending(), "\033\\[1;33m.\033\\[0m") 
+  output <-
+    if (testthat:::rcmd_running() || !any(term %in% colour_terms, na.rm = TRUE))
+      "^\\.$"
+    else "^\033\\[1;33m.\033\\[0m$"
+  expect_output(pending(), output) 
 })
