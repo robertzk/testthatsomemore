969c14fd108ae367d377093b362c4079e451f7bb
RobertZK <technoguyrob@gmail.com>
2015-03-31 14:12:40 -0500

    working parse_time helper

--- R/pretend_now_is.R
+++ R/pretend_now_is.R
@@ -56,6 +56,7 @@ parse_time.character <- function(time) {
   unit   <- normalize_unit(tolower(unit))
   op     <- if (tolower(tense) == "from now") `+` else `-`
 
+  list2env(legal_unit_number_pair(unit, number), environment())
   op(Sys.time(), as.difftime(number, units = unit))
 }
 
@@ -68,9 +69,23 @@ normalize_unit <- function(unit) {
   else unit
 }
 
+legal_unit_number_pair <- function(unit, number) {
+  if (unit == "months") {
+    unit   <- "days"
+    number <- 30 * number
+  } else if (unit == "years") {
+    unit   <- "days"
+    number <- 365 * number
+  } else if (unit == "eons") {
+    unit   <- "days"
+    number <- 99999999 * number
+  }
+  list(unit = unit, number = number)
+}
+
 extract_time <- function(matches, time) {
   setNames(nm = c("number", "unit", "tense"),
-    Map(substring, text, s <- attr(matches,"capture.start"),
+    Map(substring, time, s <- attr(matches,"capture.start"),
         s + attr(matches, "capture.length") - 1)
   )
 }
--- tests/testthat/test-pretend_now_is.R
+++ tests/testthat/test-pretend_now_is.R
@@ -1,5 +1,26 @@
 context("pretend_now_is")
 
+describe("parse_time", {
+  test_that("it leaves POSIXct alone", {
+    expect_equal(parse_time(Sys.time()), Sys.time())
+  })
+
+  test_that("it leaves Date less alone", {
+    expect_equal(parse_time(Sys.Date()), as.POSIXct(Sys.Date()))
+  })
+
+  test_that("it can parse every format in the world", {
+    grid <- expand.grid(1:10, c("day", "days", "second", "secs", "month", "years"), c("+", "-"))
+    apply(grid, 1, function(row) {
+      string <- sprintf("%s %s %s", row[1], row[2], if (row[3] == "+") "from now" else "ago")
+      list2env(legal_unit_number_pair(normalize_unit(row[2]), as.integer(row[1])), environment())
+      expect_equal(parse_time(string), 
+        getFunction(row[3])(Sys.time(), as.difftime(number, units = unit)),
+        info = sprintf("%s did not parse correctly", sQuote(string)), tolerance = 0.01)
+    })
+  })
+})
+
 test_that("it is able to pretend now is a day from today", {
   now      <- Sys.time()
   tomorrow <- now + as.difftime(1, units = "days")
