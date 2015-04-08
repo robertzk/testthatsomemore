044980a16a15f94f72241c74a07c007a92519fa9
RobertZK <technoguyrob@gmail.com>
2015-04-08 18:55:41 -0500

    use utils explicitly

--- R/stub.R
+++ R/stub.R
@@ -62,14 +62,14 @@ package_stub <- function(package_name, function_name, stubbed_value, expr) {
          getNamespace(package_name))
   if (!exists(function_name, envir = namespaces[[1]], inherits = FALSE))
     namespaces <- namespaces[-1]
-  if (!exists(function_name, envir = tail(namespaces,1)[[1]], inherits = FALSE))
+  if (!exists(function_name, envir = utils::tail(namespaces,1)[[1]], inherits = FALSE))
     stop(gettextf("Cannot stub %s::%s because it must exist in the package",
          package_name, function_name))
 
   lapply(namespaces, unlockBinding, sym = function_name)
 
   # Clean up our stubbing on exit
-  previous_object <- get(function_name, envir = tail(namespaces,1)[[1]])
+  previous_object <- get(function_name, envir = utils::tail(namespaces,1)[[1]])
   on.exit({
     lapply(namespaces, function(ns) {
       tryCatch(error = function(.) NULL, assign(function_name, previous_object, envir = ns))
