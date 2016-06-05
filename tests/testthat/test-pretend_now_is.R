fba0f251f47cde6d42b3e8af0c2ac7b88ae55f25
Peter Hurford <peter@peterhurford.com>
2016-06-05 15:09:49 -0500

    Delegate tests

--- tests/testthat/test-pretend_now_is.R
+++ tests/testthat/test-pretend_now_is.R
@@ -19,15 +19,8 @@ describe("parse_time", {
     expect_equal(parse_time(Sys.Date()), as.POSIXct(Sys.Date()))
   })
 
-  test_that("it can parse every format in the world", {
-    grid <- expand.grid(1:10, c("day", "days", "second", "secs", "minutes", "month", "years", "eons"), c("+", "-"))
-    apply(grid, 1, function(row) {
-      string <- sprintf("%s %s %s", row[1], row[2], if (row[3] == "+") "from now" else "ago")
-      list2env(legal_unit_number_pair(normalize_unit(row[2]), as.integer(row[1])), environment())
-      expect_equal(parse_time(string), 
-        getFunction(row[3])(Sys.time(), as.difftime(number, units = unit)),
-        info = sprintf("%s did not parse correctly", sQuote(string)), tolerance = 0.1)
-    })
+  test_that("it can parse date strings", {
+    expect_equal(parse_time("1 day from now"), strdate::strdate("1 day from now"))
   })
 })
 
