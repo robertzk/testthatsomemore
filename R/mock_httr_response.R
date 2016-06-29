a3334f4cc2e54cf9811b126e2e40ab8871b7f93a
danilito19 <danalitovsky@gmail.com>
2016-06-29 09:20:31 -0500

    add mock httr

---
+++ R/mock_httr_response.R
@@ -0,0 +1,31 @@
+#' Mocks an httr response
+#'
+#' @param status code integer.
+#' @param content list. 
+#' @param type character. json or text
+#' @return a response object.
+#' @export
+#' @examples
+#' mock_httr_response(200L, list(data = "it works!"), "json")
+
+mock_httr_response <- function(status_code, content, type) {
+  type <- match.arg(type, c("json", "text"))
+  if (identical(type, "json")) {
+    if(is.list(content)) { content <- jsonlite::toJSON(content, auto_unbox = TRUE) }
+    httr:::response(
+      status_code = status_code,
+      content = charToRaw(content),
+      headers = list(`Content-Type` = "application/json")
+    )
+  } else if (identical(type, "text")) {
+    httr:::response(
+      status_code = status_code,
+      content = charToRaw(content),
+      headers = list(`Content-Type` = "text")
+    )
+  } else {
+    stop(paste("Unknown Content-Type:", type))
+  }
+}
+
+
